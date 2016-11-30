f1564541aeea3cada269ecfb1a5c4c4a12d02d98
robinhosl2008 <robinhosl2008@gmail.com>
2016-11-30 00:13:51 -0200

    atualizando relatório.

--- relatorio-hd.php
+++ relatorio-hd.php
@@ -63,6 +63,31 @@
         <progress class="progress" style='width: 500px' max='<?php echo $discoTotal2; ?>' value='<?php echo $totalUsado2; ?>'></progress>
     </div>
 
+
+
 <?php
+
+echo foldersize("/var/www/sites/loja");
+
+function foldersize($dir){
+    $count_size = 0;
+    $count = 0;
+    $dir_array = scandir($dir);
+    foreach($dir_array as $key=>$filename){
+        if($filename!=".." && $filename!="."){
+            if(is_dir($dir."/".$filename)){
+                $new_foldersize = foldersize($dir."/".$filename);
+                $count_size = $count_size + $new_foldersize;
+            }else if(is_file($dir."/".$filename)){
+                $count_size = $count_size + filesize($dir."/".$filename);
+                $count++;
+            }
+        }
+
+    }
+
+    return $count_size;
+}
+
     require_once("rodape.php");
 ?>
